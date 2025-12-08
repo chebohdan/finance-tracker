003e807723d61e61a34b75cfbbfea6cc7395422d
chebohdan <bvasenshev@gmail.com>
2025-12-08 14:41:49 +0100

    ByUserId Spec now searches in userAccounts

--- finance-tracker-backend/src/main/java/com/example/financetracker/specifictation/AccountSpecifications.java
+++ finance-tracker-backend/src/main/java/com/example/financetracker/specifictation/AccountSpecifications.java
@@ -1,8 +1,8 @@
 package com.example.financetracker.specifictation;
 
-import com.example.financetracker.model.Account;
-import com.example.financetracker.model.Account_;
-import com.example.financetracker.model.User_;
+import com.example.financetracker.model.*;
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.JoinType;
 import org.springframework.data.jpa.domain.Specification;
 
 public class AccountSpecifications {
@@ -10,7 +10,12 @@ public class AccountSpecifications {
     }
 
     public static Specification<Account> byUserId(Long userId) {
-        return ((root, query, criteriaBuilder) -> criteriaBuilder.equal(
-                root.get(Account_.owner).get(User_.id), userId));
+        return ((root, query, cb) -> {
+            Join<Account, UserAccount> accountUserJoin = root.join(Account_.USER_ACCOUNTS, JoinType.LEFT);
+            return cb.or(
+                    cb.equal(root.get(Account_.OWNER).get(User_.ID), userId),
+                    cb.equal(accountUserJoin.get(UserAccount_.USER).get(User_.ID), userId)
+            );
+        });
     }
 }
